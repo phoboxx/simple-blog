30256832a6845eb7c7b4ebe802f99a89ca7c4c79
Frederic Legault <legault.f123@gmail.com>
2020-08-02 13:27:59 -0400

    Now able to connect to DB

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) return console.log('Successfuly conencted to DB');
+    console.error(err);
+  }
+);
 
 app.listen(3000, () => {
   console.log('simple-blog is listening on port 3000!');
