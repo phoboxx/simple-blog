bfa18dceb02da1c88b482f76b58f6062e694c86b
Frederic Legault <legault.f123@gmail.com>
2020-08-05 22:26:31 -0400

    client side is now able to post content to API

---
+++ client/createArticle.js
@@ -0,0 +1,24 @@
+const submitArticle = () => {
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+
+  const form = {
+    title: title,
+    body: body,
+    author: 'fred from FORM',
+  };
+
+  const sendForm = async () => {
+    const rawResponse = await fetch('http://127.0.0.1:3000/api/article', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    });
+    const content = await rawResponse.json();
+    console.log(content);
+  };
+
+  sendForm();
+};
